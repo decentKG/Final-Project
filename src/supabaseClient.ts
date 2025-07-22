2fa36fb58f17134e63f807fa7592469d82101602
decentKG <decentgwavava@gmail.com>
2025-07-22 09:53:22 +0200

    Updated

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://supabase.com/dashboard/project/qvwhyenvmbkkyxgoriki/settings/api-keys';  // replace this
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2d2h5ZW52bWJra3l4Z29yaWtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyMjA0NTMsImV4cCI6MjA2Nzc5NjQ1M30.7mjbUhsUcn6AhKE2ItLDO9s1IXjP5EgBE6dGzN2kWTo';                    // replace this
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
